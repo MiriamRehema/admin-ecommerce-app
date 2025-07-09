2dc1128e247a0508f78ea49f5cb1c9ce145ae050
Miriam Rehema <rehemamiriam30@gmail.com>
2025-07-09 09:26:43 +0200

    created the permisssion seeder

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $permissions=[
+            'role-list',
+            'role-edit',
+            'role-delete',
+            'role-create',
+            'product-list',
+            'product-create',
+            'product-edit',
+            'product-delete'
+        ];
+        foreach($permissions as $key=>$permission){
+            Permission::create(['name' => $permission]);
+        }
+    }
+}
