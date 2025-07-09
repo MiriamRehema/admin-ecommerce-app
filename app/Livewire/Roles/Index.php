ce93096b339c938b96b8d09081e7ec44a39a4996
Miriam Rehema <rehemamiriam30@gmail.com>
2025-07-09 09:27:07 +0200

    created the index.php

---
+++ app/Livewire/Roles/Index.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Livewire\Roles;
+use Spatie\Permission\Models\Role;
+
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+       
+        return view('livewire.roles.index',[
+            'roles' => Role::all()
+        ]);
+        
+    }
+}
