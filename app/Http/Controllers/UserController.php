a3fff5127658abed95af584c88d6351989a50894
Miriam Rehema <rehemamiriam30@gmail.com>
2025-07-10 09:41:08 +0200

    removed the $user from coontroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,8 +15,8 @@ class UserController extends Controller
     public function index()
     {
        
-       $users=User::all();
-        return view('users.index',compact('users'));
+       
+        return view('users.index');
     }
 
     /**
