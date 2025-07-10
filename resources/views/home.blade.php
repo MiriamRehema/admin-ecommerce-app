0523cf78924a837b9b6874eb99372a09f2e28989
Miriam Rehema <rehemamiriam30@gmail.com>
2025-07-10 09:43:54 +0200

    changed homeblade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="max-w-xl mx-auto py-10">
+    <x-card>
+        <x-slot name="title">
+            {{ __('Dashboard') }}
+        </x-slot>
+        @if (session('status'))
+            <x-notification icon="success" class="mb-4">
+                {{ session('status') }}
+            </x-notification>
+        @endif
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+        <div class="text-gray-700 text-lg">
+            {{ __('You are logged in!') }}
         </div>
-    </div>
+    </x-card>
 </div>
 @endsection
