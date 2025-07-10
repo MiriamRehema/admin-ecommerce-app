4fa39dbc8a253b9ef2e22b255998d59a3492282d
Miriam Rehema <rehemamiriam30@gmail.com>
2025-07-10 09:48:43 +0200

    created acreate blade for roles

---
+++ resources/views/Roles/create.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header">{{ __('Create Role') }}</div>
+
+                <div class="card-body">
+                    <a href="{{ route('roles.index') }}" class="btn btn-info">Back</a>
+                    <form method="POST" action="{{route('roles.store')}}">
+                    @csrf
+
+                    
+                    
+                    <div class="mt-2">
+                        <label>Name:</label>
+                        <input type="name" name="name" class="form-control">
+                        @error('name')
+                        
+                        <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="mt-2">
+                        <h3>Permission:</h3>
+                    </div>
+                    <div class="mt-2">
+                    <button class="btn btn-success">Submit</button>
+                    </div>
+                    </form>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
