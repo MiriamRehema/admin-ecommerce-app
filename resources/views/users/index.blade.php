6b6d75783428124613b6bb9d01a447c1d177030b
Miriam Rehema <rehemamiriam30@gmail.com>
2025-07-10 09:49:03 +0200

    changed indexblade for users

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,54 +1,6 @@
-@extends('layouts.app')
+ @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-10">
-            <div class="card">
-                <div class="card-header">{{ __('Users') }}</div>
 
-
-                <div class="card-body">
-                   @if (session('success'))
-                    <div class="alert alert-success">
-                    {{ session('success') }}
-                    </div>
-                    @endif
-                    <a href="{{ route('users.create') }}" class="btn btn-success mb-3">Add User</a>
-                
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Created At</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($users as $user)
-                                <tr>
-                                    <td>{{ $user->id }}</td>
-                                    <td>{{ $user->name }}</td>
-                                    <td>{{ $user->email }}</td>
-                                    <td>{{ $user->created_at->format('Y-m-d H:i') }}</td>
-                                    <td>
-                                        <form method="POST" action="{{route('users.destroy', $user->id)}}" >
-                                            @csrf
-                                            @method('DELETE')
-                                        <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary btn-sm">Edit</a>
-                                        <a href="{{ route('users.show', $user->id) }}" class="btn btn-info btn-sm">Show</a>  
-                                        <button class="btn btn-danger btn-sm">Delete</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                   
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+<livewire:users.index /> 
+@endsection 
