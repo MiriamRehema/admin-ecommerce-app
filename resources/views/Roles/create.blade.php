404c281bd35e0424162368bcc09b1b8856c4fcfd
Miriam Rehema <rehemamiriam30@gmail.com>
2025-07-11 10:03:36 +0200

    changed for roles too

--- resources/views/Roles/create.blade.php
+++ resources/views/Roles/create.blade.php
@@ -1,40 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-10">
-            <div class="card">
-                <div class="card-header">{{ __('Create Role') }}</div>
 
-                <div class="card-body">
-                    <a href="{{ route('roles.index') }}" class="btn btn-info">Back</a>
-                    <form method="POST" action="{{route('roles.store')}}">
-                    @csrf
+<div class="max-w-2xl mx-auto py-8">
+    <x-card title="Create Role">
+        <x-slot name="slot">
+            <a href="{{ route('roles.index') }}">
+                <x-button icon="arrow-left" class="mb-4" label="Back" />
+            </a>
 
-                    
-                    
-                    <div class="mt-2">
-                        <label>Name:</label>
-                        <input type="name" name="name" class="form-control">
-                        @error('name')
-                        
-                        <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
-                    <div class="mt-2">
-                        <h3>Permission:</h3>
-                    </div>
-                    <div class="mt-2">
-                    <button class="btn btn-success">Submit</button>
-                    </div>
-                    </form>
-                    </div>
-                   
+            <form method="POST" action="{{ route('roles.store') }}" class="space-y-6">
+                @csrf
+
+                <div>
+                    <x-input label="Name" name="name" value="{{ old('name') }}" />
+                    @error('name')
+                        <div class="text-red-600 text-sm mt-1">{{ $message }}</div>
+                    @enderror
                 </div>
-            </div>
-        </div>
-    </div>
+
+                <div>
+                    <h3 class="text-lg font-medium">Permissions:</h3>
+                    <!-- Add your permissions checkboxes or UI elements here -->
+                </div>
+
+                <div>
+                    <x-button type="submit" positive label="Create" />
+                </div>
+            </form>
+        </x-slot>
+    </x-card>
 </div>
 
 @endsection
