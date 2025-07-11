26c7cc6a0c63e15b25512c0fd9c149ad3665be14
Miriam Rehema <rehemamiriam30@gmail.com>
2025-07-11 10:01:38 +0200

    changed create blade for products

--- resources/views/Products/create.blade.php
+++ resources/views/Products/create.blade.php
@@ -2,16 +2,17 @@
 
 @section('content')
 <div class="max-w-2xl mx-auto py-8">
-    <x-card title="Create Product">
+    <x-modal-card title="Create Product" name="createProductModal">
         <x-slot name="slot">
             <a href="{{ route('products.index') }}">
                 <x-button icon="arrow-left" class="mb-4" label="Back" />
             </a>
+
             <form method="POST" action="{{ route('products.store') }}" class="space-y-6">
                 @csrf
 
                 <div>
-                    <x-input label="Name" name="name" value="{{ old('name') }}" />
+                    <x-input icon="shopping-bag" label="Name" name="name" placeholder="Product" />
                     @error('name')
                         <div class="text-red-600 text-sm mt-1">{{ $message }}</div>
                     @enderror
@@ -22,6 +23,10 @@
                 </div>
             </form>
         </x-slot>
-    </x-card>
+
+        <x-slot name="footer" class="flex justify-end">
+            <x-button flat label="Cancel" x-on:click="close" />
+        </x-slot>
+    </x-modal-card>
 </div>
 @endsection
