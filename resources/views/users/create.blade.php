bb771627ca8e15a830ce43c78c453519022bb0f9
Miriam Rehema <rehemamiriam30@gmail.com>
2025-07-11 10:04:37 +0200

    changed create blade for users

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,56 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-10">
-            <div class="card">
-                <div class="card-header">{{ __('Create User') }}</div>
+<div class="max-w-2xl mx-auto py-8">
+    <x-card title="Create User" name="cardModal">
+        <x-slot name="slot">
+            <a href="{{ route('users.index') }}">
+                <x-button icon="arrow-left" class="mb-4" label="Back" />
+            </a>
+            <form method="POST" action="{{ route('users.store') }}" class="space-y-6">
+                @csrf
 
-                <div class="card-body">
-                    <a href="{{ route('users.index') }}" class="btn btn-info">Back</a>
-                    <form method="POST" action="{{route('users.store')}}">
-                    @csrf
-
-                    
-                    
-                    <div class="mt-2">
-                        <label>Name:</label>
-                        <input type="name" name="name" class="form-control">
-                        @error('name')
-                        <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
-                    <div class="mt-2">
-                        <label>Email:</label>
-                        <input type="email" name="email" class="form-control">
-                        @error('email')
-                        <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
-                    <div class="mt-2">
-                        <label>Password:</label>
-                        <input type="password" name="password" class="form-control">
-                        @error('password')
-                        <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
-                    <div class="mt-2">
-                    <button class="btn btn-success">Submit</button>
-                    </div>
-                    </form>
-
-
-
-                    </div>
-
-
-                
-                   
-                   
+                <div>
+                     <x-input icon="user" label="Name" placeholder='Name' />
+                    @error('name')
+                        <div class="text-red-600 text-sm mt-1">{{ $message }}</div>
+                    @enderror
                 </div>
-            </div>
-        </div>
-    </div>
+
+                <div>
+                    <x-input icon='envelope'  label="Email" placeholder='Email' />
+                    @error('email')
+                        <div class="text-red-600 text-sm mt-1">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div>
+                    <x-password label="Password" value="I love WireUI ❤️" />
+                    
+                    @error('password')
+                        <div class="text-red-600 text-sm mt-1">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div>
+                    <x-button type="submit" positive label="Submit" />
+                </div>
+            </form>
+        </x-slot>
+    </x-card>
 </div>
 @endsection
