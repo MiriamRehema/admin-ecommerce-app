1c156c2a8ece25fb4e71e674992a830572ef1c0a
Miriam Rehema <rehemamiriam30@gmail.com>
2025-07-16 15:33:53 +0200

    added the checkbox and Put method

--- resources/views/Roles/edit.blade.php
+++ resources/views/Roles/edit.blade.php
@@ -2,40 +2,55 @@
 
 @section('content')
 <div class="max-w-2xl mx-auto py-8">
-    <h1 class="text-2xl font-semibold">Update Role</h1>
-    <a href="{{ route('roles.index') }}">
-        <x-button icon="arrow-left" class="mb-4" label="Back" />
-    </a>
+    <x-card title="Update Role">
+        <x-slot name="slot">
+            <a href="{{ route('roles.index') }}">
+              <x-button icon="arrow-left" class="mb-4" label="Back" />
+            </a>
+            
+            <form method="POST" action="{{ route('roles.update',  $role->id) }}" class="space-y-6">
+             @csrf
+             @method('PUT')
+             
 
-    <form method="POST" action="{{ route('roles.store') }}" class="space-y-6">
-        @csrf
-
-        <div >
-            <x-input icon="users" label="Role" name="role" placeholder="Role" value="{{$role->name}}"/>
-            @error("role")
+            <div >
+            <label>Name:</label>
+            <x-input icon="users" name="roles" placeholder="Role" value="{{$role->name}}"/>
+            @error("roles")
                 <div class="text-red-600 text-sm mt-1">{{ $message }}</div>
             @enderror
-        </div>
+            </div>
        
-        <div>
-        <h3 class="text-2xl font-semibold">Permissions:</h3>
-        @foreach($permissions as $permission)
-        <label>
-            {{ $permission->name }}
-            <x-checkbox  name="permissions[]" value="{{ $permission->id}}"
-           {{ $role->hasPermissionTo($permission->id) ?'checked':''}}
-            
-        </label>
-        @endforeach
-        </div>
+            <div>
+            <h3 class="text-2xl font-semibold">Permissions:</h3>
+            @foreach($permissions as $permission)
+            <label class="flex items-center">
+              <span class="ml-2">
+              <x-checkbox  
+                name="permissions[{{ $permission->name }}]" 
+                value="{{ $permission->name }}"
+                :checked="$role->hasPermissionTo($permission->name)" 
+                   />
+              </span>
+               {{ $permission->name }}
+            </label>
+            @endforeach
+            </div>
         
 
         
 
-        <div>
+             <div>
             <x-button type="submit" positive label="Update" />
-        </div>
-    </form>
+             </div>
+            </form>
+
+        </x-slot>
+
+    </x-card>
+        
+
+    
     
 </div>
 @endsection
