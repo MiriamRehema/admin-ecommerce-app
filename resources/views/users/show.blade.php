e78ae2d7cd6ecbe34da86f0da4534399e462805c
Miriam Rehema <rehemamiriam30@gmail.com>
2025-07-21 10:02:47 +0200

    added the show functionality for the users

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -12,6 +12,16 @@
             <div class="space-y-4">
                 <p><strong>Name:</strong> {{ $user->name }}</p>
                 <p><strong>Email:</strong> {{ $user->email }}</p>
+                <h4>Roles:</h4>
+                @if($user->roles->isEmpty())
+                    <p>No roles assigned</p>
+                @else
+                    <ul>
+                        @foreach($user->roles as $role)
+                            <li>{{ $role->name }}</li>
+                        @endforeach
+                    </ul>
+                @endif
                 <!-- Add more user details here as needed -->
             </div>
         </x-slot>
