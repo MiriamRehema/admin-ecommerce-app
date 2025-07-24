4e59a17f5361d5ce3f8d53b2957bb33ab887cef3
Miriam Rehema <rehemamiriam30@gmail.com>
2025-07-24 16:22:07 +0200

    added categories views

--- app/Livewire/Categories/Index.php
+++ app/Livewire/Categories/Index.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Livewire\Categories;
+use App\Models\Category;
 
 use Livewire\Component;
 
@@ -8,6 +9,8 @@ class Index extends Component
 {
     public function render()
     {
-        return view('livewire.categories.index');
+        return view('livewire.categories.index', [
+            'categories' => Category::all()
+        ]);
     }
 }
