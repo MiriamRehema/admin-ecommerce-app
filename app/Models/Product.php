1d9f2dee021e0c5282fb0ad8167bd021795f6b77
Miriam Rehema <rehemamiriam30@gmail.com>
2025-07-24 16:22:41 +0200

    added the products attributes

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable=['name'];
+    protected $fillable=[
+        'name',
+        'slug',
+        'description',
+        'image',
+        'price',
+        'is_active',
+        'category_id',
+        'stock',
+        'is_featured',
+        'is_new',
+        'is_on_sale',
+        'sale_price'
+    ];
+    protected $casts = [
+        'image' => 'array',
+        'price' => 'decimal:2',
+        'sale_price' => 'decimal:2',
+        'is_active' => 'boolean',
+        'is_featured' => 'boolean',
+        'is_new' => 'boolean',
+        'is_on_sale' => 'boolean',
+    ];
 }
