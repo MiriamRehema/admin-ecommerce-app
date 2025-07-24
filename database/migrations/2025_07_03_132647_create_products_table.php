d90820ada286187305b4873ec5dae9efcc66d0c3
Miriam Rehema <rehemamiriam30@gmail.com>
2025-07-24 16:21:18 +0200

    added more columns

--- database/migrations/2025_07_03_132647_create_products_table.php
+++ database/migrations/2025_07_03_132647_create_products_table.php
@@ -14,6 +14,17 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('slug')->unique();
+            $table->longText('description')->nullable();
+            $table->json('image')->nullable();
+            $table->decimal('price', 10, 2)->default(true);
+            $table->boolean('is_active')->default(true);
+            $table->foreignId('category_id')->contsrained('categories')->CascadeOnDelete();
+            $table->integer('stock')->default(0);
+            $table->boolean('is_featured')->default(false);
+            $table->boolean('is_new')->default(false);
+            $table->boolean('is_on_sale')->default(false);
+            
             $table->timestamps();
         });
     }
