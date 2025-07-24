03806babf28c0f6121b1412e0ed9109ed7dfc5aa
Miriam Rehema <rehemamiriam30@gmail.com>
2025-07-24 16:24:56 +0200

    added products attributes for creating a product

--- resources/views/Products/create.blade.php
+++ resources/views/Products/create.blade.php
@@ -16,6 +16,67 @@
                 <div class="text-red-600 text-sm mt-1">{{ $message }}</div>
             @enderror
         </div>
+        <div>
+            <x-input icon="tag" label="Slug" name="slug" placeholder="product-slug" />
+            @error('slug')
+                <div class="text-red-600 text-sm mt-1">{{ $message }}</div>
+            @enderror
+        </div>
+        <div>
+            <x-input label="Description" name="description" placeholder="Product description" />
+            @error('description')
+                <div class="text-red-600 text-sm mt-1">{{ $message }}</div>
+            @enderror
+        </div>
+        <div>
+            <x-input label="Price" name="price" type="number" step="0.01" placeholder="Product price" />
+            @error('price')
+                <div class="text-red-600 text-sm mt-1">{{ $message }}</div>
+            @enderror
+        </div>
+        <div>
+            <x-input label="Stock" name="stock" type="number" placeholder="Available stock" />
+            @error('stock')
+                <div class="text-red-600 text-sm mt-1">{{ $message }}</div>
+            @enderror
+        </div>
+        <div>
+            <x-toggle id="is_active" name="is_active" label="Is Active" />
+            @error('is_active')
+                <div class="text-red-600 text-sm mt-1">{{ $message }}</div>
+            @enderror
+
+        </div>
+        <div>
+            <x-select
+                label="Category"
+                name="category_id"
+                placeholder="Select a category"
+                :options="$categories->pluck('name', 'id')->toArray()"
+            />
+            @error('category_id')
+                <div class="text-red-600 text-sm mt-1">{{ $message }}</div>
+            @enderror
+        </div>
+        <div>
+            <x-toggle id="is_featured" name="is_featured" label="Is Featured" />
+            @error('is_featured')
+                <div class="text-red-600 text-sm mt-1">{{ $message }}</div>
+            @enderror
+        </div>
+        <div>
+            <x-toggle id="is_new" name="is_new" label="Is New" />
+            @error('is_new')
+                <div class="text-red-600 text-sm mt-1">{{ $message }}</div>
+            @enderror
+        </div>
+        <div>
+            <x-toggle id="is_on_sale" name="is_on_sale" label="Is On Sale" />
+            @error('is_on_sale')
+                <div class="text-red-600 text-sm mt-1">{{ $message }}</div>
+            @enderror
+        </div>
+        
 
         <div>
             <x-button type="submit" positive label="Create" />
