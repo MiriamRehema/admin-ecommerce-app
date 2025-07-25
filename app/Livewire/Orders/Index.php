58c1b4af77c38c81d4b061be5d1e3c2f6ac81e4b
Miriam Rehema <rehemamiriam30@gmail.com>
2025-07-25 15:56:28 +0200

    added all the related models

--- app/Livewire/Orders/Index.php
+++ app/Livewire/Orders/Index.php
@@ -2,6 +2,10 @@
 
 namespace App\Livewire\Orders;
 use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Product;
+use App\Models\Category;
+use App\Models\User;
 
 use Livewire\Component;
 
@@ -10,7 +14,11 @@ class Index extends Component
     public function render()
     {
         return view('livewire.orders.index', [
-            'orders' => Order::all()
+            'orders' => Order::all(),
+            'orderitems' => OrderItem::all(),
+            'products' => Product::all(),
+            'categories' => Category::all(),
+            'users' => User::all(),
         ]);
     }
 }
