84848a66181fe12a81fb0420647366bbd9c1778c
Miriam Rehema <rehemamiriam30@gmail.com>
2025-07-28 16:52:16 +0200

    added the stored requirements

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 use App\Models\Order;
+use App\Models\User;
+//use App\Models\Product;
+//use App\Models\Category;
+
+//use App\Models\OrderItem;
 
 class OrderController extends Controller
 {
@@ -13,6 +19,9 @@ class OrderController extends Controller
     public function index()
     {
         return view('orders.index');
+        
+       
+        
     }
 
     /**
@@ -21,6 +30,8 @@ class OrderController extends Controller
     public function create()
     {
         return view('orders.create');
+        //dd($orders);
+
     }
 
     /**
@@ -28,7 +39,38 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //dd($request->all());
+        //$users = User::all();
+        $request->validate([
+            'user_id'=>'required|exists:users,id',
+            'grand_total'=>'required|numeric',
+            'payment_method'=>'required|string|max:255',
+            'payment_status'=>'required|string|max:255',
+
+            'status'=>'required|string|max:255',
+            'currency'=>'required|string|max:10',
+            'shipping_amount'=>'required|numeric',
+            'shipping_method'=>'required|string|max:255',
+            'notes'=>'nullable|string|max:1000',
+
+           
+        ]);
+
+        $order = Order::create([
+            'user_id' => $request->user_id,
+            'status' => $request->status,
+            'grand_total' => $request->grand_total,
+            'payment_method' => $request->payment_method,
+            'payment_status' => $request->payment_status,
+            'currency' => $request->currency,
+            'shipping_amount' => $request->shipping_amount,
+            'shipping_method' => $request->shipping_method,
+            'notes' => $request->notes,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ])->load('user'); // Load the user relationship
+
+        return redirect()->route('orders.index')->with('success', 'Order created successfully.');
     }
 
     /**
@@ -52,7 +94,7 @@ class OrderController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        
     }
 
     /**
