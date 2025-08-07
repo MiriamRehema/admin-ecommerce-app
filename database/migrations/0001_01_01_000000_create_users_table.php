349da25523508b80c5ce0ab8e2182daf42765e5b
Miriam Rehema <rehemamiriam30@gmail.com>
2025-08-07 16:22:21 +0200

    modified users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,4 +1,4 @@
- <?php
+<?php
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
@@ -12,12 +12,11 @@ class CreateUsersTable extends Migration
         Schema::dropIfExists('users');
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->bigInteger('role_id')->unsigned();
             $table->string('name');
             $table->string('email',50)->unique();
-            $table->string('username');
             $table->string('password');
-            $table->boolean('active');
+            $table->boolean('is_active')->default(true); // Assuming you want to track if the user is active
+
             $table->rememberToken();
             $table->timestamps();
 
