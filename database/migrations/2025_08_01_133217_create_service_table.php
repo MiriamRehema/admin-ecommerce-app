38541bcbebd5df58fe4d3f89c061d45d0b38c509
Miriam Rehema <rehemamiriam30@gmail.com>
2025-08-07 16:22:46 +0200

    created service table

---
+++ database/migrations/2025_08_01_133217_create_service_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('service', function (Blueprint $table) {
+            $table->id();
+             $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2)->default(0.00); // Assuming price is a decimal value
+            $table->string('status')->default('active'); // e.g., active, inactive
+            $table->string('image')->nullable(); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('service');
+    }
+};
