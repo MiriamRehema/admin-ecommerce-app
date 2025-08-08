d1efb59cb94918967fbd283f24f60b9ca6869c8c
Miriam Rehema <rehemamiriam30@gmail.com>
2025-08-08 16:38:15 +0200

    created service controller and its model

---
+++ app/Models/Service.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Service extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'name',
+        'slug',
+        'description',
+        'price',
+        'image',
+        'is_active',
+        
+
+    ];
+    protected $casts=[
+        'image'=> 'array'
+
+    ];
+    
+    public function reviews(){
+        return $this->belongsToMany(Review::class);
+    }
+    public function services(){
+        return $this->belongsToMany(Order_service::class);
+    }
+    
+}
