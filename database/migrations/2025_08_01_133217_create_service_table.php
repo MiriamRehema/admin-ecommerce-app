00fe52b840e731d274551c77408807597b559c78
Miriam Rehema <rehemamiriam30@gmail.com>
2025-08-08 16:38:29 +0200

    created service table

--- database/migrations/2025_08_01_133217_create_service_table.php
+++ database/migrations/2025_08_01_133217_create_service_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('service', function (Blueprint $table) {
             $table->id();
-             $table->string('name');
+            $table->string('name');
             $table->string('slug')->unique();
             $table->text('description')->nullable();
             $table->decimal('price', 10, 2)->default(0.00); // Assuming price is a decimal value
