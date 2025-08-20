dd1776456b706d81b659d660d3cff4fced29a845
Miriam Rehema <rehemamiriam30@gmail.com>
2025-08-20 13:51:38 +0200

    corrected the product error

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -20,6 +20,6 @@ class OrderItem extends Model
     return $this->belongsTo(Order::class);
    }
    public function product(){
-    return $this->belongsTo(Products::class);
+    return $this->belongsTo(Product::class);
    }
 }
