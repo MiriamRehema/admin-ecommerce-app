609426f61eafae0251e3cd853e9c6a23551f35fc
Miriam Rehema <rehemamiriam30@gmail.com>
2025-08-23 21:07:30 +0200

    added more on footer

--- resources/views/livewire/partials/footer.blade.php
+++ resources/views/livewire/partials/footer.blade.php
@@ -1,9 +1,9 @@
-<footer class="bg-gray-900 w-full">
+<footer class="bg-green-600 w-full">
   <div class="w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 lg:pt-20 mx-auto">
     <!-- Grid -->
     <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6">
       <div class="col-span-full lg:col-span-1">
-        <a class="flex-none text-xl font-semibold text-white dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#" aria-label="Brand">DCodeMania</a>
+        <a class="flex-none text-xl font-semibold text-white dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#" aria-label="Brand">Afrinet Telecom</a>
       </div>
       <!-- End Col -->
 
@@ -11,9 +11,9 @@
         <h4 class="font-semibold text-gray-100">Product</h4>
 
         <div class="mt-3 grid space-y-3">
-          <p><a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/categories">Categories</a></p>
-          <p><a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/products">All Products</a></p>
-          <p><a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/products">Featured Products</a></p>
+          <p><a class="inline-flex gap-x-2 text-gray-800 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/categories">Categories</a></p>
+          <p><a class="inline-flex gap-x-2 text-gray-800 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/products">All Products</a></p>
+          <p><a class="inline-flex gap-x-2 text-gray-800 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/products">Featured Products</a></p>
         </div>
       </div>
       <!-- End Col -->
@@ -22,10 +22,11 @@
         <h4 class="font-semibold text-gray-100">Company</h4>
 
         <div class="mt-3 grid space-y-3">
-          <p><a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">About us</a></p>
-          <p><a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">Blog</a></p>
+             <p><a class="inline-flex gap-x-2 text-gray-800 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">Home</a></p>
+          <p><a class="inline-flex gap-x-2 text-gray-800 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">About us</a></p>
+          <p><a class="inline-flex gap-x-2 text-gray-800 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">Blog</a></p>
 
-          <p><a class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">Customers</a></p>
+          <p><a class="inline-flex gap-x-2 text-gray-800 hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">Customers</a></p>
         </div>
       </div>
       <!-- End Col -->
@@ -51,7 +52,7 @@
 
     <div class="mt-5 sm:mt-12 grid gap-y-2 sm:gap-y-0 sm:flex sm:justify-between sm:items-center">
       <div class="flex justify-between items-center">
-        <p class="text-sm text-gray-400">© 2024 DCodeMania. All rights reserved.</p>
+        <p class="text-sm text-gray-800">© Afrinet Telecom Limited - 2025 All Rights Reserved</p>
       </div>
       <!-- End Col -->
 
